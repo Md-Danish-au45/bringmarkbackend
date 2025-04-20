681c4289bd9233e6375e30e83732d635e2594a6b
MD Danish <82753592+Md-Danish-au45@users.noreply.github.com>
2025-04-20 23:17:55 +0530

    das

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ const errorHandler = require("./middleware/errorHandler");
 dotenv.config();
 
 const app = express();
+// Manually set credentials
+const storedUser = {
+  username: "admin",
+  password: "123456",
+};
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
+// Login API
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
+  if (username === storedUser.username && password === storedUser.password) {
+    return res.status(200).json({ message: "Login successful!" });
+  } else {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+});
 // Routes
 app.use("/api", routes); // All routes start with /api
 
