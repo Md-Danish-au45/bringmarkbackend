83d93dc9508fd23dda8e65c48c503fe17a87002d
MD Danish <82753592+Md-Danish-au45@users.noreply.github.com>
2025-04-20 22:57:29 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const authRoutes = require("./authRoutes");
 const contactRoutes = require("./contactRoutes");
 
-router.use("/auth", authRoutes);
 router.use("/contact", contactRoutes);
 
 module.exports = router;
